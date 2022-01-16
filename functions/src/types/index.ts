3a6546d3e54611df6b3ebe3ba94570ee4b6116b7
kanata-naka <faubynet@gmail.com>
2022-01-17 02:35:47 +0900

    refactor

--- functions/src/types/index.ts
+++ functions/src/types/index.ts
@@ -1,13 +1,11 @@
 /** 公開範囲 */
-export namespace Restrict {
-  /** 公開範囲: 全体公開 */
-  export const RESTRICT_ALL = "0";
-  /** 公開範囲: サブページのみ */
-  export const RESTRICT_LIMITED = "1";
-  /** 公開範囲: 非公開 */
-  export const RESTRICT_PRIVATE = "2";
-}
+export const Restrict = {
+  /** 全体公開 */
+  ALL: "0",
+  /** サブページのみ */
+  LIMITED: "1",
+  /** 非公開 */
+  PRIVATE: "2",
+} as const;
 
-const restrictList = [Restrict.RESTRICT_ALL, Restrict.RESTRICT_LIMITED, Restrict.RESTRICT_PRIVATE] as const;
-
-export type Restrict = typeof restrictList[number];
+export type Restrict = typeof Restrict[keyof typeof Restrict];
